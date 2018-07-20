afae0cd51e92d6c92084e3943e35253dc7762deb
chejianchao <820992376@qq.com>
2018-07-20 21:23:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+
+class Anagram
+  def initialize(word)
+    @word = word
+    
+  end
+  
+  def match(word_list)
+    
+    word_list.select{|word|
+      @word.chars.sort.join == word.chars.sort.join
+    }
+  end
+end
